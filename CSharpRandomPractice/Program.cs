0e5de33af49da46cfb018a10ade62eeccbb8af36
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-03-30 01:14:27 +0600

    enum practice

--- CSharpRandomPractice/Program.cs
+++ CSharpRandomPractice/Program.cs
@@ -2,11 +2,22 @@
 
 namespace CSharpRandomPractice
 {
+    public enum Name
+    {
+        Nitai = 1,
+        Chandra = 2,
+        Banik = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var intName =(int) Name.Banik;
+            var enumName = (Name)intName;
+            var stringName = enumName.ToString();
         }
     }
 }

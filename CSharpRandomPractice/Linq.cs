3f3fd550a2617eb460ea099fe7ae996738f059a9
Nitai Chandra Banik <nitai.banik@selise.ch>
2021-03-30 19:12:13 +0600

    array list

--- CSharpRandomPractice/Linq.cs
+++ CSharpRandomPractice/Linq.cs
@@ -1,28 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CSharpRandomPractice
 {
     public class Student
     {
-        public int StudentID { get; set; }
+        public int StudentId { get; set; }
         public string StudentName { get; set; }
         public int Age { get; set; }
     }
     public class Linq
     {
-        private readonly IList<Student> _studentList = new List<Student>()
+        private readonly IList<Student> _studentList = new List<Student>
         {
-            new() {StudentID = 1, StudentName = "John", Age = 18},
-            new() {StudentID = 2, StudentName = "Steve", Age = 15},
-            new () {StudentID = 3, StudentName = "Bill", Age = 25},
-            new () {StudentID = 4, StudentName = "Ram", Age = 20},
-            new () {StudentID = 5, StudentName = "Ron", Age = 19},
-            new () {StudentID = 6, StudentName = "Ram", Age = 18}
+            new() {StudentId = 1, StudentName = "John", Age = 18},
+            new() {StudentId = 2, StudentName = "Steve", Age = 15},
+            new () {StudentId = 3, StudentName = "Bill", Age = 25},
+            new () {StudentId = 4, StudentName = "Ram", Age = 20},
+            new () {StudentId = 5, StudentName = "Ron", Age = 19},
+            new () {StudentId = 6, StudentName = "Ram", Age = 18}
         };
+
+        readonly IList _mixedList = new ArrayList();
+        
+       
 
         public void PracticeLinq()
         {
+            _mixedList.Add(0);
+
+            _mixedList.Add("One");
+            _mixedList.Add("Two");
+            _mixedList.Add(3);
+            _mixedList.Add(new Student() { StudentId = 1, StudentName = "Bill" });
+
+            var stringResult = _mixedList.OfType<string>();
             var res = _studentList.OrderBy(s => s.StudentName)
                 .ThenBy(s => s.Age).ToList();
         }
